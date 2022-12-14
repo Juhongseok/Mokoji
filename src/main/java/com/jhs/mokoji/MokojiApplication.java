08f7e4878869587188860154143f24a3b22520e1
JHS <wnghdtjr129@naver.com>
2022-12-14 15:05:26 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/jhs/mokoji/MokojiApplication.java
@@ -0,0 +1,13 @@
+package com.jhs.mokoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokojiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokojiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhs/mokoji/MokojiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhs.mokoji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokojiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
