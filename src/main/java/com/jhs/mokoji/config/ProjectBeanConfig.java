f593068686b38688329fba7fe4baff531969f385
JHS <wnghdtjr129@naver.com>
2022-12-26 18:17:36 +0900

    fix: 반환타입 UserDetailsService -> UserService 변경

--- src/main/java/com/jhs/mokoji/config/ProjectBeanConfig.java
+++ src/main/java/com/jhs/mokoji/config/ProjectBeanConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -20,14 +19,14 @@ public class ProjectBeanConfig {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService());
+        provider.setUserDetailsService(userService());
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        return new UserService(userRepository);
+    public UserService userService() {
+        return new UserService(userRepository, passwordEncoder());
     }
 
     @Bean
