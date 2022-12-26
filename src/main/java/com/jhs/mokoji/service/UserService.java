32e65264b3e3e9a8722a03a1071085d907e554bb
JHS <wnghdtjr129@naver.com>
2022-12-26 18:16:04 +0900

    feat: 유저정보 저장 로직 구현

--- src/main/java/com/jhs/mokoji/service/UserService.java
+++ src/main/java/com/jhs/mokoji/service/UserService.java
@@ -1,16 +1,20 @@
 package com.jhs.mokoji.service;
 
 import com.jhs.mokoji.auth.CustomUser;
+import com.jhs.mokoji.controller.request.UserSignUpRequest;
+import com.jhs.mokoji.domain.User;
 import com.jhs.mokoji.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -18,5 +22,10 @@ public class UserService implements UserDetailsService {
                 userRepository.findUserById(username)
                     .orElseThrow(() -> new UsernameNotFoundException("Wrong userId"))
         );
+    }
+
+    public void save(UserSignUpRequest request) {
+        request.encryptionPassword(passwordEncoder);
+        userRepository.save(User.of(request));
     }
 }
