a7164be0295058384b7ff63f354ff5597217e7ff
JHS <wnghdtjr129@naver.com>
2023-01-19 15:19:43 +0900

    feat: 모임 생성 시 다대다 중간테이블 내 저장 기능 추가

--- src/main/java/com/jhs/mokoji/domain/GroupRole.java
+++ src/main/java/com/jhs/mokoji/domain/GroupRole.java
@@ -1,7 +1,7 @@
 package com.jhs.mokoji.domain;
 
 public enum GroupRole {
-    ROLE_REGULAR, ROLE_ASSOCIATE, ROLE_UN_APPROVED;
+    ROLE_REGULAR, ROLE_ASSOCIATE, ROLE_UN_APPROVED, ROLE_MANAGER;
 
     public boolean isNewMember() {
         return this.equals(GroupRole.ROLE_UN_APPROVED);
