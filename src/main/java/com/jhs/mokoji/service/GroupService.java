b64f03c480fafea97d2b93342a8fc69701aeea41
JHS <wnghdtjr129@naver.com>
2023-01-19 15:20:19 +0900

    feat: 모임 생성 시 다대다 중간테이블 내 저장 기능 추가

--- src/main/java/com/jhs/mokoji/service/GroupService.java
+++ src/main/java/com/jhs/mokoji/service/GroupService.java
@@ -1,9 +1,11 @@
 package com.jhs.mokoji.service;
 
+import com.jhs.mokoji.auth.CustomUser;
 import com.jhs.mokoji.controller.request.GroupCreateRequest;
 import com.jhs.mokoji.controller.response.GroupMember;
 import com.jhs.mokoji.domain.Group;
 import com.jhs.mokoji.domain.User;
+import com.jhs.mokoji.domain.UserGroup;
 import com.jhs.mokoji.repository.GroupRepository;
 import com.jhs.mokoji.repository.UserGroupRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,18 +16,20 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 @RequiredArgsConstructor
 public class GroupService {
 
     private final GroupRepository groupRepository;
     private final UserGroupRepository userGroupRepository;
 
-    @Transactional
-    public void add(GroupCreateRequest request) {
-        groupRepository.save(Group.of(request));
+    public void add(GroupCreateRequest request, CustomUser user) {
+        Group group = Group.of(request);
+        groupRepository.save(group);
+        userGroupRepository.save(UserGroup.manager(user, group));
     }
 
+    @Transactional(readOnly = true)
     public List<GroupMember> getMembers(Long groupId) {
         return userGroupRepository.findAllByIdGroupId(groupId)
                 .stream()
